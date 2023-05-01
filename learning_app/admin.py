f0ef2edd0106bd0087e543871879689b113e3520
YoussefElmoudene <elmoudene.ysf@gmail.com>
2023-05-01 22:03:01 +0100

    add student to superadmin

--- learning_app/admin.py
+++ learning_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from learning_app.models import Student
+
+
 # Register your models here.
+@admin.register(Student)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
