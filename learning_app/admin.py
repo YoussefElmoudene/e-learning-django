d1827e152a86d7caec6a2ba7135ce5613d7b00d6
Benattik Amal <amalbenattik7@gmail.com>
2023-05-05 22:20:15 +0100

    admin

--- learning_app/admin.py
+++ learning_app/admin.py
@@ -1,9 +1,49 @@
 from django.contrib import admin
 
-from learning_app.models import Student
+from learning_app.models import User, Student, Teacher, Course, Inscription, Chapter
 
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=('name', 'phone', 'email', 'password', 'role')
+    ordering=('name',)
+    list_filter=('name', 'email')
+    search_fields=('name',)
+
 @admin.register(Student)
-class AuthorAdmin(admin.ModelAdmin):
-    pass
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('level',)
+    ordering=('level',)
+    list_filter=('level',)
+    search_fields=('level',)
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display=('salaire', 'grade', 'specialty')
+    ordering=('salaire',)
+    list_filter=('grade',)
+    search_fields=('specialty',)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display=('title', 'level', 'description', 'dateCreated', 'duration', 'price')
+    ordering=('level',)
+    list_filter=('level',)
+    search_fields=('title',)
+
+@admin.register(Inscription)
+class InscriptionAdmin(admin.ModelAdmin):
+    list_display=('date',)
+    ordering=('date',)
+    list_filter=('date',)
+    search_fields=('date',)
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display=('name', 'description', 'img', 'video')
+    ordering=('name',)
+    list_filter=('name',)
+    search_fields=('name',)
+
+admin.site.register(User, UserAdmin)
